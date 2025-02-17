21f8cc7ba76fef2ac3e99f11c065f7315d408f92
Alessandro Florio <alessandro.florio@tol.info>
2025-02-17 16:52:07 +0100

    qt fixes

--- testApp/example.cpp
+++ testApp/example.cpp
@@ -8,7 +8,7 @@ int main(int argc, char * argv[])
 
     QQmlApplicationEngine engine;
     engine.addImportPath(QStringLiteral(PLUGIN_IMPORT_PATH));
-
+    engine.addImportPath(":/");
     const QUrl url(QStringLiteral("qrc:/example.qml"));
     engine.load(url);
 
