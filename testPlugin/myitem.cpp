f7d0f93691ace6b4fc2ec1d3d9bdcff1b7d348e1
Alessandro Florio <alessandro.florio@tol.info>
2025-03-07 13:42:55 +0100

    cleanup
    also set TARGET_NAME variable

--- testPlugin/myitem.cpp
+++ testPlugin/myitem.cpp
@@ -10,13 +10,11 @@ MyItem::MyItem(QObject * parent)
 
 void MyItem::registerTypes(const char * uri)
 {
-    // not reached in Android
     qDebug() << "MyItem called registerTypes";
 }
 
 void MyItem::initializeEngine(QQmlEngine * engine, const char * /*uri*/)
 {
-    // not reached in Android
     qDebug() << "MyItem called initializeEngine";
 
     engine->addImageProvider("testimage", new TestImageProvider());
